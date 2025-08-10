d3921ddb306b39be03d5601bbc5a6c290b72e8c4
Luiqbd <jcoxete@gmail.com>
2025-08-10 20:45:35 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Inicia o primeiro bot em segundo plano
+echo "Iniciando bot.py..."
 python bot.py &
+
+# Inicia o segundo bot
+echo "Iniciando telegram_bot.py..."
 python telegram_bot.py
