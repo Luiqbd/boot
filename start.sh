65f46dec97e6bdec9dce750abbb5e4b7be70c3f1
Luiqbd <jcoxete@gmail.com>
2025-08-11 13:21:22 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "Iniciando bot.py..."
-python bot.py &
-
-sleep 2  # Dá um tempinho pro Flask iniciar
-
-echo "Iniciando telegram_bot.py..."
-python telegram_bot.py
+echo "Iniciando bot unificado..."
+python main.py
