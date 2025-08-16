52e69bba0c23f5bffc9be83e04d8fdcd9051d3b4
Luiqbd <jcoxete@gmail.com>
2025-08-15 22:27:25 -0300

    Update exchange_client.py

--- exchange_client.py
+++ exchange_client.py
@@ -11,9 +11,25 @@ class ExchangeClient:
         self.wallet = os.getenv("WALLET_ADDRESS")
         self.private_key = os.getenv("PRIVATE_KEY")
         self.router_address = Web3.toChecksumAddress("0xE592427A0AEce92De3Edee1F18E0157C05861564")  # Uniswap V3 Router
-        with open("uniswap_v3_router_abi.json") as f:
+
+        with open("abi/uniswap_v3_router_abi.json") as f:
             self.router_abi = json.load(f)
         self.router = self.web3.eth.contract(address=self.router_address, abi=self.router_abi)
+
+        with open("abi/erc20.json") as f:
+            self.erc20_abi = json.load(f)
+
+    def approve_token(self, token_address, amount_wei):
+        token = self.web3.eth.contract(address=token_address, abi=self.erc20_abi)
+        tx = token.functions.approve(self.router_address, amount_wei).buildTransaction({
+            "from": self.wallet,
+            "gas": 100000,
+            "gasPrice": self.web3.toWei("5", "gwei"),
+            "nonce": self.web3.eth.getTransactionCount(self.wallet),
+        })
+        signed_tx = self.web3.eth.account.sign_transaction(tx, self.private_key)
+        tx_hash = self.web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return self.web3.toHex(tx_hash)
 
     def buy_token(self, token_in, token_out, amount_in_wei):
         deadline = self.web3.eth.getBlock("latest")["timestamp"] + 300
@@ -30,6 +46,31 @@ class ExchangeClient:
         }).buildTransaction({
             "from": self.wallet,
             "value": amount_in_wei,
+            "gas": 300000,
+            "gasPrice": self.web3.toWei("5", "gwei"),
+            "nonce": self.web3.eth.getTransactionCount(self.wallet),
+        })
+
+        signed_tx = self.web3.eth.account.sign_transaction(tx, self.private_key)
+        tx_hash = self.web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return self.web3.toHex(tx_hash)
+
+    def sell_token(self, token_in, token_out, amount_in_wei):
+        self.approve_token(token_in, amount_in_wei)
+
+        deadline = self.web3.eth.getBlock("latest")["timestamp"] + 300
+
+        tx = self.router.functions.exactInputSingle({
+            "tokenIn": token_in,
+            "tokenOut": token_out,
+            "fee": 3000,
+            "recipient": self.wallet,
+            "deadline": deadline,
+            "amountIn": amount_in_wei,
+            "amountOutMinimum": 0,
+            "sqrtPriceLimitX96": 0
+        }).buildTransaction({
+            "from": self.wallet,
             "gas": 300000,
             "gasPrice": self.web3.toWei("5", "gwei"),
             "nonce": self.web3.eth.getTransactionCount(self.wallet),
