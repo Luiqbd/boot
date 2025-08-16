f69bdef2c8dd804d892eeb9a00252a3a0d413bad
Luiqbd <jcoxete@gmail.com>
2025-08-16 17:20:19 -0300

    Create test_sniper.py

---
+++ test_sniper.py
@@ -0,0 +1,7 @@
+from strategy_sniper import on_new_pair
+
+on_new_pair(
+    pair_addr="0x78b9a3e9b16391df3a379ea0c5b9c1aef4b55ab7",
+    token0="0x4200000000000000000000000000000000000006",
+    token1="0x532f27101965dd16442e59d40670faf5ebb142e4"
+)
