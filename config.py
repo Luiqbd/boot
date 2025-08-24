13f232e389c8a372b8ff6d0f719b7166e4b28642
Luiqbd <jcoxete@gmail.com>
2025-08-24 15:47:09 -0300

    Update config.py

--- config.py
+++ config.py
@@ -43,13 +43,13 @@ USDC = checksum_addr(os.getenv("USDC"), "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02
 DEXES = [
     {
         "name": "Aerodrome V2",
-        "factory": checksum_addr(os.getenv("AERO_V2_FACTORY"), "0x327Df1e6de05895d2ab08513aaDD9313Fe505d86"),
-        "router": checksum_addr(os.getenv("AERO_V2_ROUTER"), "0xcF77a3D4A6f1C6a7D5cb06B52F474BeCC5123e29"),
+        "factory": checksum_addr(os.getenv("AERO_V2_FACTORY"), "0x327Df1E6de05895d2ab08513aaDD9313Fe505d86"),
+        "router": checksum_addr(os.getenv("AERO_V2_ROUTER"), "0xcF083Be4164828f00cAE704EC15a36D711491284"),
         "type": "v2"
     },
     {
         "name": "Aerodrome V3",
-        "factory": checksum_addr(os.getenv("AERO_V3_FACTORY"), "0xaC1d73C8a497dd0217018Bf7CA6c5B6508C747B0"),
+        "factory": checksum_addr(os.getenv("AERO_V3_FACTORY"), "0x420dd381b31aef6683db6b90208442878ebf0f8d"),
         "router": checksum_addr(os.getenv("AERO_V3_ROUTER"), "0x14eBb7fc750F1107E6d36fB31A0c6B0f7F73B09F"),
         "type": "v3"
     },
@@ -62,21 +62,27 @@ DEXES = [
     {
         "name": "Uniswap V3",
         "factory": checksum_addr(os.getenv("UNI_V3_FACTORY"), "0x33128a8fC17869897Dce68Ed026d694621f6FDfD"),
-        "router": checksum_addr(os.getenv("UNI_V3_ROUTER"), "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45"),
+        "router": checksum_addr(os.getenv("UNI_V3_ROUTER"), "0x2626664c2603336E57B271c5C0b26F421741e481"),  # corrigido para Base
         "type": "v3"
     },
     {
         "name": "BaseSwap V2",
-        "factory": checksum_addr(os.getenv("BASE_V2_FACTORY"), "0x800D4F78312daC73DDa3E04cB38c45DDBEa5098B"),
-        "router": checksum_addr(os.getenv("BASE_V2_ROUTER"), "0x327d98243aEFa3F627aBEbCA63C0A6c4a258963b"),
+        "factory": checksum_addr(os.getenv("BASE_V2_FACTORY"), "0x06e0feb0d74106c7ada8497754074d222ec6e8ef"),
+        "router": checksum_addr(os.getenv("BASE_V2_ROUTER"), "0x327Df1E6de05895d2ab08513aaDD9313Fe505d86"),
         "type": "v2"
     },
     {
         "name": "BaseSwap V3",
         "factory": checksum_addr(os.getenv("BASE_V3_FACTORY"), "0x47989441fD3A19774f8aF9F21614c83Bfb4b0775"),
-        "router": checksum_addr(os.getenv("BASE_V3_ROUTER"), "0x327d98243aEfa3F627aBEbCA63C0A6c4a258963b"),
+        "router": checksum_addr(os.getenv("BASE_V3_ROUTER"), "0x327Df1E6de05895d2ab08513aaDD9313Fe505d86"),
         "type": "v3"
     },
+    {
+        "name": "SushiSwap",
+        "factory": checksum_addr(os.getenv("SUSHI_FACTORY"), "0x71524b4f93c58fcb2e0f0c5e2ada8f350b9a0212"),
+        "router": checksum_addr(os.getenv("SUSHI_ROUTER"), "0x044b75f554b886A065b9567891e45c79542d7357"),
+        "type": "v2"
+    }
 ]
 
 # ---------------------------------------------------
