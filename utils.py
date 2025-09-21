faae30a163f1cb218728a73ee9a4d3fb0f4906dc
Luiqbd <jcoxete@gmail.com>
2025-09-21 19:32:39 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,13 +18,16 @@ logger = logging.getLogger(__name__)
 def escape_md_v2(text: str) -> str:
     """
     Escapa caracteres especiais para Telegram MarkdownV2.
-    Caracteres escapados: _ * [ ] ( ) ~ ` > # + - = | { } . !
+    Caracteres escapados: _ * [ ] ( ) ~ ` > # + = - | { } . ! \
     """
-    pattern = r'([\_\*
+    # Coloque o hífen logo após o = para não interferir no range do caractere
+    pattern = r'([_*
 
 \[\]
 
-\(\)\~\`\>\#\+\-\=\|\{\}\.\!])'
+()~`>#+=\-|{}.!\\]
+
+)'
     return re.sub(pattern, r'\\\1', text)
 
-# Ratelimiter e Etherscan omitidos para brevidade...
+# Ratelimiter e funções de Etherscan omissas para brevidade…
